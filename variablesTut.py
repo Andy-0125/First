c35f25d13056c33510a4c030ea36b0a665f88f65
Andy Lu <hylu25@uwcchina.org>
2026-09-04 14:21:23 +0800

    New practice files added for learning Python.

---
+++ variablesTut.py
@@ -0,0 +1,19 @@
+first_name = "Andy"
+food = "Fish"
+
+#integer
+age = 16
+
+#float
+weight = 54.8
+
+#boolean
+is_student = True
+for_sale=False
+
+#main
+if is_student:
+    print(f"{first_name} is a student.")
+else:
+    print(f"{first_name} is not a student.")
+    print(type(is_student))
